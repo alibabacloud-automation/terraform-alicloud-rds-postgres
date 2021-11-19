e53d48577b8ff0f1b5b1a06304a8b9182fd51367
王超 <wc01169074@alibaba-inc.com>
2021-11-19 14:26:06 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/rds-postgres"
-}
-
 locals {
   engine                = "PostgreSQL"
   this_instance_id      = var.existing_instance_id != "" ? var.existing_instance_id : concat(alicloud_db_instance.this.*.id, [""])[0]
